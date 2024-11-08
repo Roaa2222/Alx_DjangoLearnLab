45ab21ee32531a4f759ea54bef43035a2ee4f052
Roaa2222 <roaa1602@gmail.com>
2024-11-08 09:28:34 -0800

    done

---
+++ django-models/relationship_app/view.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from relationship_app.models import Book
+
+def list_books(request):
+    books = Book.objects.all()  # Get all books
+    return render(request, 'relationship_app/list_books.html', {'books': books})
+from django.views.generic import DetailView
+from relationship_app.models import Library
+
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'relationship_app/library_detail.html'
+    context_object_name = 'library'  # Define the context name to be used in the template
