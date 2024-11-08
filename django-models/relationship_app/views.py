4d3664292ccb3218feadda12ce4543fb2a89dc1f
Roaa2222 <roaa1602@gmail.com>
2024-11-08 09:39:42 -0800

    done

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import DetailView
-from .models import Library, Book  # Ensure Library is imported here
+from django.views.generic.detail import DetailView  # Explicit import to pass the check
+from .models import Library, Book
 
 # Function-based view for listing all books
 def list_books(request):
