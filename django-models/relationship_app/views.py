40d9d71931e72147cb24059671d33c072baacff5
Roaa2222 <roaa1602@gmail.com>
2024-11-09 13:20:59 -0800

    done

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -4,6 +4,9 @@ from .models import UserProfile
 def is_admin(user):
     return user.is_authenticated and user.userprofile.role == UserProfile.Role.ADMIN
 
+def is_librarian(user):
+    return user.is_authenticated and user.userprofile.role == UserProfile.Role.LIBRARIAN
+
 def admin_view(request):
     # Your admin view logic here
     return render(request, 'admin_view.html')
@@ -13,7 +16,7 @@ def librarian_view(request):
     # Your librarian view logic here
     return render(request, 'librarian_view.html')
 
-@user_passes_test(lambda u: u.is_authenticated and u.userprofile.role == UserProfile.Role.MEMBER)
+@user_passes_test(is_librarian)
 def member_view(request):
     # Your member view logic here
     return render(request, 'member_view.html')
