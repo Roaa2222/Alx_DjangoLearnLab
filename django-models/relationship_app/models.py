f5acccfafe5999d981f6c34a6c877c60c3099e28
Roaa2222 <roaa1602@gmail.com>
2024-11-09 11:05:47 -0800

    done

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -1,28 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-class Author(models.Model):
-    name = models.CharField(max_length=100)
+class UserProfile(models.Model):
+    # Define the role choices
+    ROLE_CHOICES = [
+        ('Admin', 'Admin'),
+        ('Librarian', 'Librarian'),
+        ('Member', 'Member')
+    ]
 
-    def __str__(self):
-        return self.name
+    # One-to-One relationship with the User model
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
 
-class Book(models.Model):
-    title = models.CharField(max_length=200)
-    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
-
-    def __str__(self):
-        return self.title
-
-class Library(models.Model):
-    name = models.CharField(max_length=150)
-    books = models.ManyToManyField(Book, related_name='libraries')
-
-    def __str__(self):
-        return self.name
-
-class Librarian(models.Model):
-    name = models.CharField(max_length=100)
-    library = models.OneToOneField(Library, on_delete=models.CASCADE, related_name='librarian')
+    # Role field with predefined choices: Admin, Librarian, and Member
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES)
 
     def __str__(self):
-        return self.name
+        return f"{self.user.username} - {self.role}"
+
+# Signal to create or update the UserProfile when a User is created or saved
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.userprofile.save()
