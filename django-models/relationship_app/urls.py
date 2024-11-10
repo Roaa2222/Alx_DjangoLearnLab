af1aaafca3437fd94587c10b1fecbfd7dc971f63
Roaa2222 <roaa1602@gmail.com>
2024-11-10 14:58:14 -0800

    done

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,11 +1,10 @@
-# relationship_app/urls.py
 from django.urls import path
-from . import views
+from .views import list_books, LibraryDetailView, admin_view, librarian_view, member_view
 
 urlpatterns = [
-    # URL pattern for the function-based view
-    path('books/', views.list_books, name='list_books'),
-    
-    # URL pattern for the class-based view
-    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
+    path('books/', list_books, name='list_books'),
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+    path('admin_view/', admin_view, name='admin_view'),
+    path('librarian_view/', librarian_view, name='librarian_view'),
+    path('member_view/', member_view, name='member_view'),
 ]
