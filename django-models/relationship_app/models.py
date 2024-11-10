503807bdd9c9eabbca817fc5c4e7f6d9e2884f32
Roaa2222 <roaa1602@gmail.com>
2024-11-10 15:59:46 -0800

    done

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -3,6 +3,15 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
+from django.db import models
+
+class Author(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
 class UserProfile(models.Model):
     # Define the role choices with only Admin and Member
     ROLE_CHOICES = [
