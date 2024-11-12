36b4e0c920086aa7c3c12136464d3322d4fc8b1a
Roaa2222 <133399768+Roaa2222@users.noreply.github.com>
2024-11-12 08:32:29 +0200

    Update models.py
    "done"

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -28,6 +28,7 @@ class UserProfile(models.Model):
     # Define the role choices with only Admin and Member
     ROLE_CHOICES = [
         ('Admin', 'Admin'),
+        ('Librarian', 'Librarian'),
         ('Member', 'Member')
     ]
 
@@ -35,7 +36,7 @@ class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
     # Role field with predefined choices: Admin and Member
-    role = models.CharField(max_length=20, choices=ROLE_CHOICES)
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES,default='Member')
 
     def __str__(self):
         return f"{self.user.username} - {self.role}"
