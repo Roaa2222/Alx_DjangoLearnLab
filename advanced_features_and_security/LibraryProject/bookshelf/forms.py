fc4fc9143a3d63888acf5e7a17a4355196ffd0c1
Roaa2222 <133399768+Roaa2222@users.noreply.github.com>
2024-11-16 03:17:39 +0200

    Update forms.py
    "done"

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -1,5 +1,12 @@
 # bookshelf/forms.py
 from django import forms
+from .models import Book  # Import your model if you need a ModelForm
+
+class ExampleForm(forms.Form):
+    # Replace these fields with the ones relevant to your use case
+    title = forms.CharField(max_length=100, required=True)
+    author = forms.CharField(max_length=100, required=True)
+    published_date = forms.DateField(required=False)
 
 class BookSearchForm(forms.Form):
     search = forms.CharField(max_length=100, required=False)
