57e4f6b3e51b05e45aae3e83a88f81205840c7f4
Roaa2222 <133399768+Roaa2222@users.noreply.github.com>
2024-11-23 23:04:08 +0200

    Update views.py
    "done"

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,7 +1,14 @@
 from rest_framework.generics.ListAPIView import ListAPIView
+from rest_framework.viewsets import ModelViewSet
 from .models import Book
 from .serializers import BookSerializer
 
 class BookList(ListAPIView):
     queryset = Book.objects.all()  # Fetch all books
     serializer_class = BookSerializer  # Use the serializer for the Book model
+class BookViewSet(ModelViewSet):
+    """
+    A viewset for viewing, editing, creating, and deleting Book instances.
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
