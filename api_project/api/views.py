dce2511312dd65b33c835b04f86561119ed41f64
Roaa2222 <133399768+Roaa2222@users.noreply.github.com>
2024-11-24 18:34:36 +0200

    Update views.py
    "done"

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -2,6 +2,7 @@ from rest_framework.generics.ListAPIView import ListAPIView
 from rest_framework.viewsets.ModelViewSet import ModelViewSet
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework.permissions import IsAuthenticated
 
 class BookList(ListAPIView):
     queryset = Book.objects.all()  # Fetch all books
@@ -10,5 +11,10 @@ class BookViewSet(ModelViewSet):
     """
     A viewset for viewing, editing, creating, and deleting Book instances.
     """
+ class MyModelViewSet(ModelViewSet):
+    queryset = MyModel.objects.all()
+    serializer_class = MyModelSerializer
+    permission_classes = [IsAuthenticated]
+     
     queryset = Book.objects.all()
     serializer_class = BookSerializer
