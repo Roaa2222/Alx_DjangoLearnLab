5096b71d561c8dbfe8362f9d116c004836a83b2a
Roaa2222 <133399768+Roaa2222@users.noreply.github.com>
2024-12-02 00:45:03 +0200

    Update models.py
    "done"

--- advanced-api-project/api/models.py
+++ advanced-api-project/api/models.py
@@ -13,6 +13,8 @@ class Book(models.Model):
     title = models.CharField(max_length=255)
     publication_year = models.IntegerField()
     author = models.ForeignKey(Author, related_name='books', on_delete=models.CASCADE)
-
+ @property
+    def publication_year(self):
+        return self.published_date.year
     def __str__(self):
         return self.title
