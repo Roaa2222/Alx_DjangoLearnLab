4784311a063ec9a02fa92eeeb36a9f2fadaf8419
Roaa2222 <133399768+Roaa2222@users.noreply.github.com>
2024-12-05 04:47:44 +0200

    Update settings.py
    "done"

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -78,8 +78,15 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'your_db_name',
+        'USER': 'your_username',
+        'PASSWORD': 'your_password',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
-}
+    }
+
 
 
 # Password validation
