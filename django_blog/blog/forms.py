a9f8be77765297e60611c2a459f57c5c10dd7707
Roaa2222 <133399768+Roaa2222@users.noreply.github.com>
2024-12-06 23:51:00 +0200

    Update forms.py
    "done"

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,11 +1,20 @@
 from django import forms
 from .models import Post
 from .models import Comment
-
+from taggit.forms import TagWidget
 # File: blog/forms.py
 
 from taggit.forms import TagField
 
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'content', 'tags']
+        widgets = {
+            'tags': TagWidget(attrs={
+                'placeholder': 'Add tags separated by commas',
+            }),
+        }
 class PostForm(forms.ModelForm):
     tags = TagField(required=False)  # Allow adding tags
 
