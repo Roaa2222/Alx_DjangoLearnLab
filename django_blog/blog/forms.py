74fa24b60bdbd48f6a585e9a296202972774111b
Roaa2222 <133399768+Roaa2222@users.noreply.github.com>
2024-12-06 23:45:15 +0200

    Update forms.py
    "done"

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -2,6 +2,18 @@ from django import forms
 from .models import Post
 from .models import Comment
 
+# File: blog/forms.py
+
+from taggit.forms import TagField
+
+class PostForm(forms.ModelForm):
+    tags = TagField(required=False)  # Allow adding tags
+
+    class Meta:
+        model = Post
+        fields = ['title', 'content', 'tags']
+
+
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
