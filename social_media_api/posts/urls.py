863e3025b1376a6b6f2bf5008f627d02064a862e
Roaa2222 <133399768+Roaa2222@users.noreply.github.com>
2024-12-14 00:15:17 +0200

    Update urls.py
    "done"

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -4,5 +4,5 @@ from .views import PostViewSet, CommentViewSet
 router = DefaultRouter()
 router.register(r'posts', PostViewSet)
 router.register(r'comments', CommentViewSet)
-
+router.register(r'feeds',FeedViewSet)
 urlpatterns = router.urls
