68c64f4f833eba725f551a64df17b517601e2d82
Roaa2222 <133399768+Roaa2222@users.noreply.github.com>
2024-12-13 13:56:20 +0200

    Update urls.py
    "done"

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from  .views import RegisterView, UserProfileView
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns=[
   path('register/', RegisterView.as_view(), name='register'),
     path('profile/', UserProfileView.as_view(), name='profile'),
-  path('login/',,name='login'),
+  path('login/',obtain_auth_token,name='login'),
 ]
