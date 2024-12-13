a5722fbc3d328950c99c8f7335970360e9471703
Roaa2222 <133399768+Roaa2222@users.noreply.github.com>
2024-12-13 23:41:18 +0200

    Update views.py
    "done"

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -5,6 +5,29 @@ from rest_framework.permissions import AllowAny
 from rest_framework import status
 from .serializers import RegisterSerializer, UserSerializer
 from .models import CustomUser
+from django.shortcuts import get_object_or_404
+
+
+class FollowUserView(APIView):
+    def post(self, request, user_id):
+        user_to_follow = get_object_or_404(CustomUser, id=user_id)
+        if user_to_follow == request.user:
+            return Response({"detail": "You cannot follow yourself."}, status=status.HTTP_400_BAD_REQUEST)
+        request.user.following.add(user_to_follow)
+        return Response({"detail": f"You are now following {user_to_follow.username}."}, status=status.HTTP_200_OK)
+
+class UnfollowUserView(APIView):
+    def post(self, request, user_id):
+        user_to_unfollow = get_object_or_404(CustomUser, id=user_id)
+        if user_to_unfollow == request.user:
+            return Response({"detail": "You cannot unfollow yourself."}, status=status.HTTP_400_BAD_REQUEST)
+        request.user.following.remove(user_to_unfollow)
+        return Response({"detail": f"You have unfollowed {user_to_unfollow.username}."}, status=status.HTTP_200_OK)
+
+
+
+
+
 
 class RegisterView(APIView):
     permission_classes = [AllowAny]
