c4ea27ff48f7ad9393ecbc3d0bd4623d6ffbd166
Roaa2222 <133399768+Roaa2222@users.noreply.github.com>
2024-12-13 23:38:08 +0200

    Update models.py
    "done"

--- social_media_api/posts/models.py
+++ social_media_api/posts/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.contrib.auth.models import AbstractUser
+
 
 User = get_user_model()
 
@@ -12,6 +14,16 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+        
+class CustomUser(AbstractUser):
+    bio = models.TextField(blank=True, null=True)
+    profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True, null=True)
+    followers = models.ManyToManyField(
+        'self', symmetrical=False, related_name='following', blank=True
+    )
+
+    def __str__(self):
+        return self.username
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
