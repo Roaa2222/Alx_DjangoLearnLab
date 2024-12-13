23a7a24ec2863c9dbe6dd87a8bd3999c9a8efefc
Roaa2222 <133399768+Roaa2222@users.noreply.github.com>
2024-12-13 23:39:14 +0200

    Update serializers.py
    "done"

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -4,6 +4,15 @@ from django.contrib.auth.password_validation import validate_password
 from rest_framework.authtoken.models import Token
 
 
+class FollowSerializer(serializers.ModelSerializer):
+    followers_count = serializers.IntegerField(source='followers.count', read_only=True)
+    following_count = serializers.IntegerField(source='following.count', read_only=True)
+
+    class Meta:
+        model = CustomUser
+        fields = ['id', 'username', 'followers_count', 'following_count']
+
+
 User =serializers.CharField(), get_user_model().objects.create_user
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
