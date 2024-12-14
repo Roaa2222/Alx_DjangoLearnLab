fa2b827ca658d391f66eb6addba8e18cc7c502b9
Roaa2222 <133399768+Roaa2222@users.noreply.github.com>
2024-12-14 19:39:36 +0200

    Update models.py
    "done"

--- social_media_api/posts/models.py
+++ social_media_api/posts/models.py
@@ -1,9 +1,18 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import AbstractUser
-
+from django.contrib.auth.models import User
 
 User = get_user_model()
+
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='likes')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='likes')
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('user', 'post')
+
 
 class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
@@ -11,6 +20,7 @@ class Post(models.Model):
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
