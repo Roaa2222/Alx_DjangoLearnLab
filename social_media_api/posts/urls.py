7544c03ec27c34c083d73212de00ff181b45568f
Roaa2222 <133399768+Roaa2222@users.noreply.github.com>
2024-12-14 20:03:39 +0200

    Update urls.py
    "done"

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -1,10 +1,13 @@
 from rest_framework.routers import DefaultRouter
 from .views import PostViewSet, CommentViewSet
+from .views import LikePostView, UnlikePostView
 
 router = DefaultRouter()
 router.register(r'posts', PostViewSet)
 router.register(r'comments', CommentViewSet)
 urlpatterns = [
     path('feed/', FeedView.as_view(), name='feed'),
+    path('<int:pk>/like/', LikePostView.as_view(), name='like-post'),
+    path('<int:pk>/unlike/', UnlikePostView.as_view(), name='unlike-post'),
 ]
 urlpatterns = router.urls
